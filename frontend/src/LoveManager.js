8757ca48970579421bb28b7875153064ab485046
Michael Coffey <eartle@gmail.com>
2014-01-13 16:07:49 +0000

    Fixed love and unloving tracks.

--- frontend/src/LoveManager.js
+++ frontend/src/LoveManager.js
@@ -2,11 +2,16 @@ module.exports = function(socket, currentTrackDao, chat, lastfmClient) {
 	var that = this;
 	
 	function update(user, unloveFlag) {
-		lastfmClient.setLoveStatus(user, currentTrackDao.getCurrentTrack(), unloveFlag, function(err) {
+		lastfmClient.setLoveStatus(user, currentTrackDao.getCurrentTrack(), !unloveFlag, function(err) {
 			if (err) {
 				console.log("Error: " + err);
 			} else {
-				chat.userLoved(user);
+				if (unloveFlag) {
+					chat.userUnloved(user);
+				}
+				else {
+					chat.userLoved(user);
+				}
 			}
 		});
 		currentTrackDao.updateLoveFlag(user.username, !unloveFlag);
