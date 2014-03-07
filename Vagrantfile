1db38d0e4492b143f5eb5c70cc9c5305d6a42031
Richard Jones <rj@metabrew.com>
2014-03-07 16:48:12 +0000

    use haproxy, in ssl-only mode

--- Vagrantfile
+++ Vagrantfile
@@ -46,6 +46,8 @@ Vagrant.configure("2") do |config|
     config.vm.network :public_network #, ip: "10.0.0.10"
     config.ssh.forward_agent = true
 
+    config.vm.synced_folder "~/Dropbox/clientroomradio/", "/home/vagrant/dropbox"
+
     recipes = ["basebox", "clientroomradio"]
     config.vm.provision "chef_solo" do |chef|
         recipes.each { |r| chef.add_recipe r }
--- cookbooks/clientroomradio/recipes/default.rb
+++ cookbooks/clientroomradio/recipes/default.rb
@@ -1,7 +1,33 @@
 include_recipe "basebox"
 
+directory "/etc/clientroomradio" do
+    owner "root"
+    action :create
+end
+
+file "/etc/clientroomradio/crr.pem" do
+    owner "root"
+    group "root"
+    mode 0644
+    action :create
+    content ::File.open("/home/vagrant/dropbox/crr.pem").read
+end
+
 include_recipe "clientroomradio::node"
 include_recipe "clientroomradio::libspotify"
 include_recipe "clientroomradio::redis"
+include_recipe "clientroomradio::haproxy"
+
+
+AFW.create_rule(node,
+        "Allow http",
+        {'table' => 'filter',
+        'rule'  => '-A INPUT -p tcp --dport 80 -j ACCEPT'
+        })
+AFW.create_rule(node,
+        "Allow http",
+        {'table' => 'filter',
+        'rule'  => '-A INPUT -p tcp --dport 443 -j ACCEPT'
+        })
 
 
