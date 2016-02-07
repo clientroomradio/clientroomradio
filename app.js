ff3c9ad0fb8bb6b05b17f63a42dec14a6ea496b4
Michael Coffey <eartle@gmail.com>
2016-02-07 18:21:48 +0000

    don't use internal http requests for messages anymore

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var DataStore = require("./lib/DataStore.js");
 var EndOfDayRequestManager = require("./lib/EndOfDayRequestManager.js");
 var DiscoveryHourRequestManager = require("./lib/DiscoveryHourRequestManager.js");
 var ExpressExternal = require("./lib/ExpressExternal.js");
-var ExpressInternal = require("./lib/ExpressInternal.js");
 var ExternalHttpServer = require("./lib/ExternalHttpServer.js");
 var FrontendUpdater = require("./lib/FrontendUpdater.js");
 var HeartbeatManager = require("./lib/HeartbeatManager.js");
@@ -21,7 +20,6 @@ var ScrobblingManager = require("./lib/ScrobblingManager.js");
 var SkipManager = require("./lib/SkipManager.js");
 var Socket = require("./lib/Socket.js");
 var Spotify = require("./lib/Spotify.js");
-var SpotifyRequestIssuer = require("./lib/SpotifyRequestIssuer.js");
 var UserActivityFlagManager = require("./lib/UserActivityFlagManager.js");
 var UserDao = require("./lib/UserDao.js");
 var VotingManager = require("./lib/VotingManager.js");
@@ -42,7 +40,6 @@ var currentTrackDao = new CurrentTrackDao(dataStore, socket, logger);
 var chat = new Chat(socket, config);
 var skipManager = new SkipManager(socket, chat);
 var progressManager = new ProgressManager(socket);
-var expressInternal = new ExpressInternal(config, chat, progressManager, logger);
 var expressExternal = new ExpressExternal(config, lastfmClient, userDao, chat, permissionChecker, logger);
 var externalHttpServer = new ExternalHttpServer(expressExternal, socket, config, logger);
 var votingManager = new VotingManager(chat, socket, dataStore);
@@ -50,18 +47,16 @@ var heartbeatManager = new HeartbeatManager(socket, chat, userDao);
 
 // Nothing depends on those:
 
-new Backend(dataStore, lastfmClient, spotify, skipManager, config, logger);
+new Backend(dataStore, lastfmClient, spotify, skipManager, socket, chat, progressManager, logger);
 new FrontendUpdater(socket, userDao, currentTrackDao, skipManager, dataStore);
 new ScrobblingManager(socket, userDao);
 new LoveManager(socket, currentTrackDao, chat, lastfmClient, logger);
 new CurrentTrackChatUpdater(currentTrackDao, chat);
-new SpotifyRequestIssuer(chat, socket, config, logger);
 new EndOfDayRequestManager(userDao, votingManager, socket);
 new DiscoveryHourRequestManager(votingManager, socket, dataStore);
 new UserActivityFlagManager(userDao, chat, socket);
 
 // Start
-expressInternal.start();
 expressExternal.start();
 externalHttpServer.start();
 heartbeatManager.start();
