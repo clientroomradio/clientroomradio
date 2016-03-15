ddb3e4341fa0ec6ca8bfda49f1222a3b1e354568
Michael Coffey <eartle@gmail.com>
2016-03-15 08:46:34 +0000

    Fix loving tracks
    We were still using the old track format when doing a love

--- test/LastfmClientTest.js
+++ test/LastfmClientTest.js
@@ -14,6 +14,7 @@ describe("LastfmClient", () => {
         mockLogger,
         mockDataStore,
         mockLasfm,
+        capturedParams,
         lastfmClient;
 
     beforeEach(() => {
@@ -33,8 +34,13 @@ describe("LastfmClient", () => {
             record: () => {}
         };
 
+        capturedParams = {};
+
         mockLasfm = {
-            request: chai.spy()
+            request: chai.spy((method, params) => {
+                capturedParams = params;
+                params.handlers.success({});
+            })
         };
 
         lastfmClient = new LastfmClient(mockConfig, mockLogger, mockDataStore, mockLasfm);
@@ -71,6 +77,42 @@ describe("LastfmClient", () => {
             skippers.push("test-user");
             lastfmClient.scrobble(track, scrobbleUsers, skippers);
             expect(mockLasfm.request).to.not.have.been.called;
+        });
+    });
+
+    describe("#setLoveStatus()", () => {
+        var track, user;
+
+        beforeEach(() => {
+            track = {
+                artists: [{name: "artist"}],
+                name: "title"
+            };
+
+            user = {
+                username: "test-user",
+                sk: "sk"
+            };
+        });
+
+        it("should call track.love when asked to love", (done) => {
+            lastfmClient.setLoveStatus(user, track, true, () => {
+                expect(mockLasfm.request).to.have.been.called.with("track.love");
+                expect(capturedParams).to.have.property("track", "title");
+                expect(capturedParams).to.have.property("artist", "artist");
+                expect(capturedParams).to.have.property("sk", "sk");
+                done();
+            });
+        });
+
+        it("should call track.unlove when asked to unlove", (done) => {
+            lastfmClient.setLoveStatus(user, track, false, () => {
+                expect(mockLasfm.request).to.have.been.called.with("track.unlove");
+                expect(capturedParams).to.have.property("track", "title");
+                expect(capturedParams).to.have.property("artist", "artist");
+                expect(capturedParams).to.have.property("sk", "sk");
+                done();
+            });
         });
     });
 });
